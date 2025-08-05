243381e171721e911ef8443a6fd628b96967ca8d
Connor Shorten <connorshorten300@gmail.com>
2025-08-05 16:43:45 -0400

    Update looping_query_writer.py

--- retrieve_dspy/retrievers/looping_query_writer.py
+++ retrieve_dspy/retrievers/looping_query_writer.py
@@ -1,9 +1,18 @@
+from typing import Optional
+
 from retrieve_dspy.retrievers.base_rag import BaseRAG
 from retrieve_dspy.models import DSPyAgentRAGResponse
 
 class LoopingQueryWriter(BaseRAG):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(
+        self,
+        collection_name: str,
+        target_property_name: Optional[str] = "content",
+        verbose: Optional[bool] = False,
+        search_only: Optional[bool] = True,
+        retrieved_k: Optional[int] = 20
+    ):
+        super().__init__(collection_name, target_property_name, search_only=search_only, verbose=verbose, retrieved_k=retrieved_k)
 
     def forward(self, question: str) -> DSPyAgentRAGResponse:
         pass
