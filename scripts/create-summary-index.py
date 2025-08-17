b2f82015fc0507014b296502c6c6f700ef68932a
Connor Shorten <connorshorten300@gmail.com>
2025-08-17 13:29:17 -0400

    add `weaviate-client[agents]` dependency, update TA script

--- scripts/create-summary-index.py
+++ scripts/create-summary-index.py
@@ -3,8 +3,7 @@ import weaviate
 from weaviate.classes.init import Auth
 from weaviate.agents.transformation import TransformationAgent
 from weaviate.agents.classes import Operations
-from weaviate.classes.config import Property, DataType, Configure
-
+from weaviate.classes.config import DataType
 
 client = weaviate.connect_to_weaviate_cloud(
     cluster_url=os.environ.get("WEAVIATE_URL"),
@@ -14,20 +13,19 @@ client = weaviate.connect_to_weaviate_cloud(
 collection = client.collections.get("EnronEmails")
 
 add_summary = Operations.update_property(
-    property_name="summary",
-    data_type=DataType.TEXT,
-    view_properties=["docs_text"],
-    instruction="""Create a summary of the document.
-    The summary should be a single sentence.
-    The summary should be a single sentence."""
+    property_name="email_summary",
+    view_properties=["email_body"],
+    instruction="""Your task is to summarize the information contained in this email.
+    Please be careful not to miss any important information. It is very important that your summary is accurate and factual.
+    """
 )
 
 agent = TransformationAgent(
     client=client,
-    collection="FreshstackLangchain",
+    collection="EnronEmails",
     operations=[add_summary],
 )
 
 response = agent.update_all()
 
-agent.get_status(workflow_id=response.workflow_id)
+print(agent.get_status(workflow_id=response.workflow_id))
