d171ba374bec00cda273ac5b699ed0ed4bf3ad8d
Connor Shorten <connorshorten300@gmail.com>
2025-10-22 10:05:51 -0400

    Update eval_utils.py

--- retrieve_dspy/benchmark_run/eval_utils.py
+++ retrieve_dspy/benchmark_run/eval_utils.py
@@ -4,7 +4,7 @@ from dspy import Example, Prediction
 from typing import List, Tuple, Dict, Callable
 import yaml
 from retrieve_dspy.clients import get_weaviate_client, get_voyage_client
-from retrieve_dspy.datasets.in_memory import in_memory_dataset_loader
+from retrieve_dspy.data_loaders.in_memory import in_memory_dataset_loader
 from retrieve_dspy.metrics import create_metric
 
 
